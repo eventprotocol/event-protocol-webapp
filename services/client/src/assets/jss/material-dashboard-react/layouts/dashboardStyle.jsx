e06975b5cf96d6c3d086c996fda412ab9d6bae74
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-08-14 02:50:11 +0800

    revert css changes

--- services/client/src/assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx
+++ services/client/src/assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx
@@ -23,7 +23,7 @@ const appStyle = theme => ({
     overflowScrolling: "touch"
   },
   content: {
-    marginTop: "35px",
+    marginTop: "70px",
     padding: "30px 25px",
     minHeight: "calc(100vh - 123px)"
   },
