3ca27d78e094ff20e68ac557ac5f35b8e6b67810
Keshik <keshik6@gmail.com>
2018-08-23 21:53:41 +0800

    Added team data json file and package-lock file

--- services/client/src/routes/dashboard.jsx
+++ services/client/src/routes/dashboard.jsx
@@ -2,6 +2,8 @@
 import Person from "@material-ui/icons/Person";
 import Dashboard from "@material-ui/icons/Dashboard";
 import BusinessCenter from "@material-ui/icons/BusinessCenter";
+import Book from "@material-ui/icons/Book";
+import Group from "@material-ui/icons/Group";
 
 
 // core components/view
@@ -9,7 +11,8 @@ import Marketplace from "../views/Marketplace/Marketplace.jsx";
 import Workspace from "../views/Workspace/Workspace.jsx";
 import UserProfile from "../views/UserProfile/UserProfile.jsx";
 import PublicProfile from "../views/PublicProfile/PublicProfile.jsx";
-
+import Resources from "../views/Resources/Resources.jsx";
+import Team from "../views/Team/Team.jsx";
 
 
 const dashboardRoutes = [
@@ -36,6 +39,23 @@ const dashboardRoutes = [
     loginRequired: true,
     component: UserProfile
   },
+  {
+    path: "/resources",
+    sidebarName: "Resources",
+    navbarName: "Resources",
+    icon: Book,
+    loginRequired: false,
+    component: Resources
+  },
+  {
+    path: "/team",
+    sidebarName: "Team",
+    navbarName: "Team",
+    icon: Group,
+    loginRequired: false,
+    component: Team
+  },
+
 
   // These are invisible sidebar items for routing purposes
   {
