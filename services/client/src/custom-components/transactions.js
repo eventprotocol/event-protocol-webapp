839ba880c8e0c6df11c7fb161354be3567f2e630
Keshik <keshik6@gmail.com>
2019-02-01 03:11:57 +0800

    Transactions mapping to event id completed. Transactions page under Wallet is completed

--- services/client/src/custom-components/transactions.js
+++ services/client/src/custom-components/transactions.js
@@ -16,6 +16,42 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import EventToken from "./EventToken.json"
+import EventContract from "./EventContract.json"
+import Contract from "./EventContract.json"
+//import get_id from "./get_contract_id.js"
+
+var Web3 = require('web3');
+var BigNumber = require('bignumber.js');
+const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/JRIhcMSUX50sCH9PKk6b"));
+const token_abi = EventToken.abi
+const event_abi = EventContract.abi
+
+const event_contract_address = "0x89e8a23ca8bab8bef769df2c10c060dc1c30053f"
+
+const contract = new web3.eth.Contract(event_abi, event_contract_address)
+
+
+
+const abi = Contract.abi
+console.log(abi)
+
+var input = "00000000000000000000000089e8a23ca8bab8bef769df2c10c060dc1c30053f00000000000000000000000000000000000000000000000ad78ebc5ac62000000000000000000000000000000000000000000000000000000000000000000001"
+// abi[2] is tokenfallback
+console.log(abi)
+console.log(web3.eth.abi.encodeFunctionSignature(abi[5]))
+
+
+// for (var i = 0; i < abi.length; i++) {
+//   // if (i === 1){
+//   //   continue;
+//   // }
+//   console.log(web3.eth.abi.encodeFunctionSignature(abi[i]))
+// }
+
+// create an instance of web3 using the HTTP provider.
+// NOTE in mist web3 is already available, so check first if it's available before instantiating
+//var web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/JRIhcMSUX50sCH9PKk6b"));
 
 const styles = theme => ({
   root: {
@@ -37,13 +73,12 @@ class Transactions extends React.Component {
         this.state = {
             value: []
         }
+        this.getContractId = this.getContractId.bind(this);
     }
 
     componentDidMount(){
       const str = 'http://api-rinkeby.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${token}'
       const obj = {address:this.props.eth_address, token:'RA5HQDQNQXD9V1FK6ZTEJYWDGYWPAEPURC'}
-
-
       const result = new Function('const {' + Object.keys(obj).join(',') + '} = this.obj;return `' + str + '`').call({obj})
 
         axios.get(result)
@@ -61,8 +96,31 @@ class Transactions extends React.Component {
 
     }
 
+    getContractId(transaction_input){
+      var event_id = "Wallet"
+      var value = 0
+      //tokenfallback function abi
+      const tokenFallback_ = "0x95f847fd"
+      const resolve_event = "0xda9db866"
+
+      var key = transaction_input.slice(0, 10)
+      var input = transaction_input.slice(10, transaction_input.length)
+
+      if (key === tokenFallback_){
+        event_id = "Event " + web3.eth.abi.decodeParameters(token_abi[10].inputs, input)[2]
+        value = new BigNumber(web3.eth.abi.decodeParameters(token_abi[10].inputs, input)[1])/(Math.pow(10, 18))
+      }
+
+      if (key === resolve_event){
+        event_id = web3.eth.abi.decodeParameters(event_abi[5].inputs, input)[0]
+      }
+
+      return [event_id, value]
+
+    }
+
     render(){
-        console.log(this.props.eth_address)
+        console.log(this.state)
         if(this.state.value === undefined)
             return null;
 
@@ -77,8 +135,10 @@ class Transactions extends React.Component {
               <TableCell>{el.timeStamp}</TableCell>
               <TableCell>{el.from}</TableCell>
               <TableCell>{el.to}</TableCell>
-              <TableCell>{el.value}</TableCell>
+              <TableCell>{this.getContractId(el.input)[1]}</TableCell>
+              <TableCell>{this.getContractId(el.input)[0]}</TableCell>
               </TableRow>
+
         ))
 
         return (
@@ -91,6 +151,7 @@ class Transactions extends React.Component {
                     <TableCell align="right">From</TableCell>
                     <TableCell align="right">To</TableCell>
                     <TableCell align="right">ET transferred</TableCell>
+                    <TableCell align="right">Transaction Type</TableCell>
                   </TableRow>
                 </TableHead>
 
